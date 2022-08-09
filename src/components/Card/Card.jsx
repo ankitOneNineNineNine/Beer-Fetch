17f466c202128fb948b8d3736d8fc061425689c1
jsankit99 <jsankit99@gmail.com>
2022-08-09 13:36:14 +0545

    fix: hover issues

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { colors } from "../../infrastructure/colors/colors";
 import { Tooltip } from "../Tolltip/Tooltip";
 import classes from "./Card.module.css";
 
@@ -13,12 +14,28 @@ import classes from "./Card.module.css";
  */
 export const Card = ({ ingredients, description, image, tagline, name }) => {
   const [showTooltip, setShowTooltip] = useState(false);
+  const containerRef = useRef(null);
   return (
-    <div className={classes["card-container"]}>
+    <div
+      className={classes["card-container"]}
+      ref={containerRef}
+      onMouseEnter={(e) =>
+        (containerRef.current.style.background = colors["dim-blue"])
+      }
+      onMouseLeave={() =>
+        (containerRef.current.style.background = colors.white)
+      }
+    >
       <div
         className={classes["image-container"]}
-        onMouseEnter={() => setShowTooltip(true)}
-        onMouseLeave={() => setShowTooltip(false)}
+        onMouseEnter={() => {
+          setShowTooltip(true);
+          containerRef.current.style.background = colors.white;
+        }}
+        onMouseLeave={() => {
+          setShowTooltip(false);
+          containerRef.current.style.background = colors["dim-blue"];
+        }}
       >
         {showTooltip && <Tooltip text={`ingredients: ${ingredients}`} />}
         <img src={image} alt={name} />
---
+++ src/infrastructure/colors/colors.js
@@ -0,0 +1,4 @@
+export const colors = {
+  "dim-blue": "rgb(220 220 255)",
+  white: "#fff",
+};
