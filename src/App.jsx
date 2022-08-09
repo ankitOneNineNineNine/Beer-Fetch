b0e16a9fe04e24cdcd75c417aba24e75bfde78d6
jsankit99 <jsankit99@gmail.com>
2022-08-09 09:39:29 +0545

    fix: loading fixed

--- src/App.jsx
+++ src/App.jsx
@@ -45,29 +45,32 @@ function App() {
 
   return (
     <div className={classes["app-container"]}>
-      {!errors && !beers?.length && (
+      {!errors && !beers?.length ? (
         <p className={classes.loading}>Loading ...</p>
+      ) : (
+        <>
+          <h4 className={classes.header}>Beers</h4>
+          {errors && <p className={classes.error}>{errors}</p>}
+          <div className={classes["list-container"]}>
+            {beers.map((beer) => {
+              return (
+                <Card
+                  key={beer.id}
+                  name={beer.name}
+                  tagline={beer.tagline}
+                  ingredients={Object.keys(beer.ingredients).join(",")}
+                  description={beer.description}
+                  image={beer.image_url}
+                />
+              );
+            })}
+          </div>
+          <div className={classes["show-more-container"]} onClick={loadMore}>
+            <p>Load More</p>
+            <i className={"fa fa-angle-down " + classes["down-icon"]} />
+          </div>
+        </>
       )}
-      <h4 className={classes.header}>Beers</h4>
-      {errors && <p className={classes.error}>{errors}</p>}
-      <div className={classes["list-container"]}>
-        {beers.map((beer) => {
-          return (
-            <Card
-              key={beer.id}
-              name={beer.name}
-              tagline={beer.tagline}
-              ingredients={Object.keys(beer.ingredients).join(",")}
-              description={beer.description}
-              image={beer.image_url}
-            />
-          );
-        })}
-      </div>
-      <div className={classes["show-more-container"]} onClick={loadMore}>
-        <p>Load More</p>
-        <i className={"fa fa-angle-down " + classes["down-icon"]} />
-      </div>
     </div>
   );
 }
